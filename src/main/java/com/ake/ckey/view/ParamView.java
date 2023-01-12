84f30da9bd0ca645c018f94d4521267d99cd3a34
keqingyuan <695774301@qq.com>
2023-01-12 14:31:57 +0800

    optimize code

--- src/main/java/com/ake/ckey/view/ParamView.java
+++ src/main/java/com/ake/ckey/view/ParamView.java
@@ -4,7 +4,6 @@ import javafx.geometry.Insets;
 import javafx.scene.control.Control;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.control.Tooltip;
 import javafx.scene.layout.HBox;
 import javafx.scene.text.TextAlignment;
 
